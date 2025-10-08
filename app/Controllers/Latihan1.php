cc51bb70617e81edf8d9a9a1b1edd4ae25943fad
dharma <dharmamahesa18@gmail.com>
2025-10-08 13:48:53 +0700

    c

--- app/Controllers/Latihan1.php
+++ app/Controllers/Latihan1.php
@@ -3,12 +3,16 @@
 namespace App\Controllers;
 
 use App\Models\ModelLatihan1;
-use CodeIgniter\Controller;
 
-class Latihan1 extends Controller
+// Ganti "use CodeIgniter\Controller;"
+class Latihan1 extends BaseController 
 {
     public function __construct()
     {
+        // [Wajib Ditambahkan] Memanggil konstruktor parent
+        parent::__construct(); 
+
+        // Inisiasi Model
         $this->ModelLatihan1 = new ModelLatihan1();
     }
 
@@ -18,11 +22,11 @@ class Latihan1 extends Controller
     }
 
     public function penjumlahan($n1, $n2)
-{
-    $data['nilai1'] = $n1;
-    $data['nilai2'] = $n2;
-    $data['hasil'] = $this->ModelLatihan1->jumlah($n1, $n2);
+    {
+        $data['nilai1'] = $n1;
+        $data['nilai2'] = $n2;
+        $data['hasil'] = $this->ModelLatihan1->jumlah($n1, $n2);
 
-    return view('view-latihan1', $data);
-}
+        return view('view-latihan1', $data);
+    }
 }
